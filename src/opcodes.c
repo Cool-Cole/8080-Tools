dd5601c2d68bbe9f4341da3695683d549b0ba7a6
Cole <git@cole.pizza>
2021-12-31 12:01:39 -0500

    Add testing ROM

--- src/opcodes.c
+++ src/opcodes.c
@@ -1942,11 +1942,35 @@ void CZ(cpuState *state) {
 
 // 0xcd
 void CALL(cpuState *state) {
+
     state->PC += 3;
 
-    state->SP -= 2;
-    writeShort(state->memory, state->SP, state->PC);
-    state->PC = readShort(state->memory, state->PC - 2);
+    if (5 == readShort(state->memory, state->PC - 2))
+    {
+        if (state->C == 9)
+        {
+            uint16_t offset = state->DE;
+            char *str = &state->memory[offset+3];  //skip the prefix bytes
+            while (*str != '$')
+                printf("%c", *str++);
+            printf("\n");
+        }
+        else if (state->C == 2)
+        {
+            //saw this in the inspected code, never saw it called
+            printf ("print char routine called\n");
+        }
+    }
+    else if (0 == readShort(state->memory, state->PC - 2))
+    {
+        exit(0);
+    }
+    else {
+
+        state->SP -= 2;
+        writeShort(state->memory, state->SP, state->PC);
+        state->PC = readShort(state->memory, state->PC - 2);
+    }
 }
 
 // 0xce
