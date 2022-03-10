05dae7eeb2980d527ca30eca56539e73c723ac0c
Cole <git@cole.pizza>
2022-03-10 12:52:49 -0500

    Create a separate source file for the snapshot capabilities

---
+++ src/handleSnapshots.h
@@ -0,0 +1,13 @@
+//
+// Created by Cole on 3/10/2022.
+//
+
+#ifndef INC_8080EMU_HANDLESNAPSHOTS_H
+#define INC_8080EMU_HANDLESNAPSHOTS_H
+
+#include <stdlib.h>
+#include "handleState.h"
+
+int takeStateSnapshot(struct cpuState *state);
+
+#endif//INC_8080EMU_HANDLESNAPSHOTS_H
