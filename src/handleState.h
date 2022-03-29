ebfb5adc7f3c706e36f0cfce49c2b7d69d0faf06
Cole <git@cole.pizza>
2022-03-29 09:02:45 -0400

    Delete snapshot files. Instead of taking state snapshots I am going to handle time travel debugging by re-runing the program.

--- src/handleState.h
+++ src/handleState.h
@@ -77,50 +77,6 @@ typedef struct cpuState {
 
 } cpuState;
 
-typedef struct cpuStateSnapshot {
-
-    // HL register
-    union {
-        uint16_t HL;
-        struct {
-            uint8_t L, H;
-        };
-    };
-
-    // DE register
-    union {
-        uint16_t DE;
-        struct {
-            uint8_t E, D;
-        };
-    };
-
-    // BC register
-    union {
-        uint16_t BC;
-        struct {
-            uint8_t C, B;
-        };
-    };
-
-    uint8_t A;
-
-    uint16_t SP;
-    uint16_t PC;
-
-    struct cpuFlags flags;
-
-    // An instruction can only change two bytes at most
-    // In the future I will make it be able to save more data at once but this is good enough for now
-    uint16_t shortAddr;
-    uint16_t shortWritten;
-
-    // TODO: An underscore followed by a capital letter is reserved, what about a lower case letter?
-    //struct cpuStateSnapshot *_nextSnapshot;
-    struct cpuStateSnapshot *_previousSnapshot;
-
-} cpuStateSnapshot;
-
 struct cpuState initState(void);
 int emulateState(struct cpuState *state);
 int dumpState(struct cpuState *state, char *filename);
